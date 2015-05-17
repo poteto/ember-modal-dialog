74437fa29928cc666ad8430d1ca67b35d1080503
Chris LoPresto <chris@chrislopresto.com>
2015-05-17 03:48:55 -0400

    Import tether helper libraries

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ module.exports = {
 
     app.import(app.bowerDirectory + '/tether/js/utils.js');
     app.import(app.bowerDirectory + '/tether/js/tether.js');
+    app.import(app.bowerDirectory + '/tether/js/abutment.js');
+    app.import(app.bowerDirectory + '/tether/js/constraint.js');
+    app.import(app.bowerDirectory + '/tether/js/markAttachment.js');
+    app.import(app.bowerDirectory + '/tether/js/shift.js');
     app.import(app.bowerDirectory + '/tether/css/tether.css');
     app.import(app.bowerDirectory + '/tether/css/tether-theme-basic.css');
   }
